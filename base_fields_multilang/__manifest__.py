48562265958447835b2682e904685e3fc167a835
Vassil Toumbev <vassy@mail.bg>
2018-05-04 02:40:34 +0300

    Update __manifest__.py

--- base_fields_multilang/__manifest__.py
+++ base_fields_multilang/__manifest__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2016-Today Prodax Ltd. (<https://www.prodax.bg>).
+#    Odoo, Open Source Management Solution
+#    (c) 2016-2018 Prodax Ltd. - Vassil Toumbev (support@openerp.bg)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
